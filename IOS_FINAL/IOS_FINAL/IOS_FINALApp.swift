96e302ed2fdef70c437bad112630cd855f43d8d2
CoarLPY <t110aem001@ntut.org.tw>
2022-06-12 14:10:39 +0800

    create project

---
+++ IOS_FINAL/IOS_FINAL/IOS_FINALApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_FINALApp.swift
+//  IOS_FINAL
+//
+//  Created by lo pui yi on 12/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct IOS_FINALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
